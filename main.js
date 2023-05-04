82628a1e1cd1bf37cbe8f53adb7e4364534806b5
Bruno <bruno2231@gmail.com>
2023-05-04 13:04:26 -0300

    Adicionando itens

---
+++ main.js
@@ -0,0 +1,62 @@
+let entradaTexto = document.getElementById('caixaEntrada')
+let saidaTexto = document.getElementById('caixaSaida')
+
+function enviarCrypt(){
+    const textoCrypt = criptografarTexto(entradaTexto.value);
+    if (entradaTexto.value == ''){
+        saidaTexto.value = 'Erro! Nenhuma mensagem encontrada para ser criptografada.'
+    }
+    else{
+        saidaTexto.value = textoCrypt
+        entradaTexto.value = ''
+    }
+}
+
+function criptografarTexto(entrada) {
+    const matrizSubstituicao = [    ['e', 'enter'],
+      ['i', 'imes'],
+      ['a', 'ai'],
+      ['o', 'ober'],
+      ['u', 'ufat']
+    ];
+  
+    let resultado = entrada;
+    for (let i = 0; i < matrizSubstituicao.length; i++) {
+      resultado = resultado.replace(new RegExp(matrizSubstituicao[i][0], "gi"), matrizSubstituicao[i][1]);
+    }
+    return resultado;
+  }
+
+  
+function enviarDecrypt(){
+    const txtDecrypt = descriptografarTexto(entradaTexto.value);
+    if (entradaTexto.value == ''){
+        saidaTexto.value = 'Erro! Nenhuma mensagem encontrada para ser descriptografada.'
+    }else{
+        saidaTexto.value = txtDecrypt
+        entradaTexto.value = ''
+    }
+} 
+
+function descriptografarTexto(entrada) {
+    const matrizSubstituicao = [    ['e', 'enter'],
+      ['i', 'imes'],
+      ['a', 'ai'],
+      ['o', 'ober'],
+      ['u', 'ufat']
+    ];
+  
+    let resultado = entrada;
+    for (let i = 0; i < matrizSubstituicao.length; i++) {
+      resultado = resultado.replace(new RegExp(matrizSubstituicao[i][1], "gi"), matrizSubstituicao[i][0]);
+    }
+    return resultado;
+  }
+
+function copiarTexto() {
+  var cpytext = document.getElementById("caixaSaida").value;
+
+  if (cpytext !== "") {
+    navigator.clipboard.writeText(cpytext);
+  }
+}
